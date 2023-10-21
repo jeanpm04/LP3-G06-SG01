36021664f759f92e3315eccb3e01deaf4fd870b3
Jean <jeanpm04@gmail.com>
2023-10-21 12:34:51 -0500

    Ejercicio 1.3.

--- LP3_lab07/Ejercicio_1/Ejercicio1.java
+++ LP3_lab07/Ejercicio_1/Ejercicio1.java
@@ -1,18 +1,24 @@
-// commit 2
+// commit 3
 public class Ejercicio1 {
 
 	
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		double[] v = new double[15];
-		acceso(v,16);
+		try {
+			acceso(v,16);
+		}
+		catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("La excepcion se manejo en main: " + e.getMessage());
+		}
 	}
 	
-	public static double acceso(double[] v, int j) throws Exception {
+	public static double acceso(double[] v, int j) {
 		try {
 			if(j>=0 && j <= v.length)
 				return v[j];
 			else
-				throw new Exception("El indice " + j + " no existe en el vector");
+				throw new ArrayIndexOutOfBoundsException("El indice " + j + 
+						" no existe en el vector");
 		}
 		catch (Exception exc) {
 			throw exc;
